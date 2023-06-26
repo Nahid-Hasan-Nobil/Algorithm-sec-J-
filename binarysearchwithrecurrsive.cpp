123bc547f9c96833da97a8253da3c900c35c4f4a
Nahid-Hasan-Nobil <135527374+Nahid-Hasan-Nobil@users.noreply.github.com>
2023-06-26 13:03:59 +0600

    binarysearchwithrecurrsive.cpp

--- binarysearchwithrecurrsive.cpp
+++ binarysearchwithrecurrsive.cpp
@@ -1,16 +1,16 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-int binarySearch(int A[], int low, int high, int x) {
+int binarySearch(int *A, int low, int high, int x) {
     if (low > high) {
         return -1;
     }
 
     int mid = (low + high) / 2;
 
-    if (x == A[mid]) {
+    if (x == *(A + mid)) {
         return mid;
-    } else if (x < A[mid]) {
+    } else if (x < *(A + mid)) {
         return binarySearch(A, low, mid - 1, x);
     } else {
         return binarySearch(A, mid + 1, high, x);
@@ -19,6 +19,7 @@ int binarySearch(int A[], int low, int high, int x) {
 
 int main() {
     int n;
+
 
     cout << "Enter the size of the array: ";
     cin >> n;
@@ -33,7 +34,6 @@ int main() {
 
     cout << "Enter the element to search for: ";
     cin >> x;
-
 
     int index = binarySearch(A, 0, n - 1, x);
 
