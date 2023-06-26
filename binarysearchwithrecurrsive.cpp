a65911db70633cf84c6e4f6ea42136e1853a15b3
Nahid-Hasan-Nobil <135527374+Nahid-Hasan-Nobil@users.noreply.github.com>
2023-06-26 12:51:51 +0600

    Add files via upload

---
+++ binarysearchwithrecurrsive.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int A[], int low, int high, int x) {
+    if (low > high) {
+        return -1;
+    }
+
+    int mid = (low + high) / 2;
+
+    if (x == A[mid]) {
+        return mid;
+    } else if (x < A[mid]) {
+        return binarySearch(A, low, mid - 1, x);
+    } else {
+        return binarySearch(A, mid + 1, high, x);
+    }
+}
+
+int main() {
+    int n;
+
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    int A[n];
+    cout << "Enter the elements of the sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cin >> A[i];
+    }
+
+    int x;
+
+    cout << "Enter the element to search for: ";
+    cin >> x;
+
+
+    int index = binarySearch(A, 0, n - 1, x);
+
+    if (index != -1) {
+        cout << "Element found at index " << index << endl;
+    } else {
+        cout << "Element not found" << endl;
+    }
+
+    return 0;
+}
---
+++ marge2array.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+void mergeArrays(int A[], int B[], int C[], int n, int m) {
+    int i = 0, j = 0, k = 0;
+
+    while (i <= n - 1 && j <= m - 1) {
+        if (A[i] > B[j]) {
+            C[k++] = B[j++];
+        } else {
+            C[k++] = A[i++];
+        }
+    }
+
+    while (i <= n - 1) {
+        C[k++] = A[i++];
+    }
+
+    while (j <= m - 1) {
+        C[k++] = B[j++];
+    }
+}
+
+int main() {
+    int n, m;
+
+    cout << "Enter the size of the first array: ";
+    cin >> n;
+
+    int A[n];
+    cout << "Enter the elements of the first array: ";
+    for (int i = 0; i < n; i++) {
+        cin >> A[i];
+    }
+
+    cout << "Enter the size of the second array: ";
+    cin >> m;
+
+    int B[m];
+    cout << "Enter the elements of the second array: ";
+    for (int i = 0; i < m; i++) {
+        cin >> B[i];
+    }
+
+
+    int mergedSize = n + m;
+
+
+    int C[mergedSize];
+
+
+    mergeArrays(A, B, C, n, m);
+
+    cout << "The merged array is: ";
+    for (int i = 0; i < mergedSize; i++) {
+        cout << C[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
